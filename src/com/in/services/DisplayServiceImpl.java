93e800a7e5ea170b6c2d12e530590e9545d24c8c
yashbakliwal01 <yashbakliwal01@gmail.com>
2025-04-23 23:07:43 +0530

    Updated code in displayserviceImpl

--- src/com/in/services/DisplayServiceImpl.java
+++ src/com/in/services/DisplayServiceImpl.java
@@ -1,6 +1,8 @@
 package com.in.services;
 
+import com.in.dto.DisplayBoard;
 import com.in.dto.ParkingEvent;
+import com.in.enums.ParkingEventType;
 import com.in.enums.ParkingSpotEnum;
 import com.in.interfaces.DisplayService;
 import com.in.interfaces.Observer;
@@ -9,14 +11,29 @@ public class DisplayServiceImpl implements DisplayService, Observer{
 
 	@Override
 	public void update(ParkingEvent event) {
-		// TODO Auto-generated method stub
 		
+		int currentCount = DisplayBoard.getInstance().getFreeParkingSpots().get(event.getParkingSpotEnum());
+		int change =0;
+		
+		if(event.getEventType().equals(ParkingEventType.ENTRY)) {
+			change = -1;
+		}
+		else {
+			change = 1;
+		}
+		
+		int newCount = change + currentCount;
+		DisplayBoard.getInstance().getFreeParkingSpots().replace(event.getParkingSpotEnum(), newCount);
+		//return;
 	}
 
 	@Override
-	public void update(ParkingSpotEnum parkingSpotEnum, int i) {
-		// TODO Auto-generated method stub
-		
+	public void update(ParkingSpotEnum parkingSpotEnum, int change) {
+
+		Integer currentCount = DisplayBoard.getInstance().getFreeParkingSpots().get(parkingSpotEnum);
+		if(currentCount == null) currentCount=0;
+		int newCount = currentCount+change;
+		DisplayBoard.getInstance().getFreeParkingSpots().replace(parkingSpotEnum, newCount);
 	}
 
 }
